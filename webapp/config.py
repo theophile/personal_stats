fbecaf3aeebce0527378e2d58666efa3f315fa0e
theophile <chmeredith@gmail.com>
2026-03-03 11:24:13 -0600

    Configure storage secret for persisted user filters

--- webapp/config.py
+++ webapp/config.py
@@ -6,3 +6,5 @@ DEFAULT_DB_PATH = Path(os.environ.get("PERSONAL_STATS_DB_PATH", "ascdatabase2.db
 DEFAULT_HOST = os.environ.get("PERSONAL_STATS_HOST", "0.0.0.0")
 DEFAULT_PORT = int(os.environ.get("PERSONAL_STATS_PORT", "8080"))
 DEFAULT_UI_RELOAD = os.environ.get("PERSONAL_STATS_UI_RELOAD", "false").lower() in {"1", "true", "yes", "on"}
+
+DEFAULT_STORAGE_SECRET = os.environ.get("PERSONAL_STATS_STORAGE_SECRET", "personal-stats-local-secret")
